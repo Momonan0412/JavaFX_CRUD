a1fbb3372b485b9dbe75da683ab7b597fa577d3c
Momonan0412 <chua.avrilnigel0412@gmail.com>
2024-04-21 21:05:14 +0800

    idk

--- src/main/java/com/example/demo/DisplayController.java
+++ src/main/java/com/example/demo/DisplayController.java
@@ -1,11 +1,28 @@
 package com.example.demo;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DisplayController implements Initializable {
+    @FXML
+    public Text txt_FirstName;
+    @FXML
+    public Text txt_LastName;
+    @FXML
+    public Text txt_UserName;
+    @FXML
+    public Text txt_Email;
+    @FXML
+    public Text txt_StudentNumber;
+    @FXML
+    public Text txt_Gender;
+    @FXML
+    public Text txt_FavoriteProgLanguage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
